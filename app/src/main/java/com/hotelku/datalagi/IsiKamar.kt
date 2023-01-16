b669a5836bdf1de4572b48559a64c4fea77a198e
michael <michael20ti@mahasiswa.pcr.ac.id>
2023-01-16 20:01:39 +0700

    InformasiKamar

---
+++ app/src/main/java/com/hotelku/datalagi/IsiKamar.kt
@@ -0,0 +1,40 @@
+package com.hotelku.datalagi
+
+class IsiKamar {
+    companion object{
+        fun setDataKamar() : ArrayList<DataKamar>{
+            val list = ArrayList<DataKamar>()
+            list.add(
+                DataKamar(
+                    "kamar01",
+                    "Superior Twin",
+                    "2 Ranjang Twin",
+                    "AC, WIFI, Sarapan Gratis, minibar, Televisi, Pembuat Teh/Kopi",
+                    "https://ik.imagekit.io/tvlk/generic-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/10003885-1296x864-FIT_AND_TRIM-574e0b9643eed1706fd6e04d7bd1f783.jpeg?_src=imagekit&tr=c-at_max,h-360,q-40,w-550",
+                    "-Rp 750.000 / malam"
+                )
+            )
+            list.add(
+                DataKamar(
+                    "kamar02",
+                    "Superior Double Bed",
+                    "1 King Bed",
+                    "AC, WIFI, Sarapan Gratis, minibar, Televisi, Pembuat Teh/Kopi",
+                    "https://ik.imagekit.io/tvlk/generic-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/10003885-79a1d98e544847420eb4fe8bdd6a8749.jpeg?_src=imagekit&tr=c-at_max,h-360,q-40,w-550",
+                    "-Rp 750.000 / malam"
+                )
+            )
+            list.add(
+                DataKamar(
+                    "kamar03",
+                    "Deluxe Double Bed",
+                    "1 King Bed",
+                    "AC, WIFI, Sarapan Gratis, minibar, Televisi, Pembuat Teh/Kopi",
+                    "https://ik.imagekit.io/tvlk/generic-asset/dgXfoyh24ryQLRcGq00cIdKHRmotrWLNlvG-TxlcLxGkiDwaUSggleJNPRgIHCX6/hotel/asset/10003885-79a1d98e544847420eb4fe8bdd6a8749.jpeg?_src=imagekit&tr=c-at_max,h-360,q-40,w-550",
+                    "Rp 935.000 / malam"
+                )
+            )
+            return list
+        }
+    }
+}
